616f210dea1f20f12733c55a2186e5a1c5c7e49a
Mubuntu <patrick.a.frison@gmail.com>
2018-01-14 00:45:18 +0100

    changed repo up, will work with discord.js

--- bot.js
+++ bot.js
@@ -1,58 +1,20 @@
-var Discord = require('discord.io');
-var logger = require('winston');
-var auth = require('./auth.json');
-var repo = 'https://github.com/Mubuntu/bimi-bot';
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(logger.transports.Console, {
-    colorize: true
+const botSettings = require("./auth.json"); //file on fs
+const Discord = require("discord.js"); //package
+var async = require('async');
+// create client
+const bot = new Discord.Client({
+    disableEveryone: true
 });
-logger.level = 'debug';
-// Initialize Discord Bot
-var bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
+
+bot.on("ready", async () => {
+    console.log(`bot is ready ${bot.user.username}`);
+
+    try {
+        let link = await bot.generateInvite(["ADMINISTRATOR"]);
+        console.log(link);
+    } catch (e) {
+        console.log(e.stack);
+    }
 });
-bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
-});
-bot.on('message', function (user, userID, channelID, message, evt) {
-    // bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-       
-        args = args.splice(1);
-        switch(cmd) {
-            // !ping
-            case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                });
-            break;
-            // Just add any case commands if you want to..
-            case 'bimi': 
-            bot.sendMessage({
-                to: channelID,
-                message: "Ma komaan!"
-            });
-            break;
-            case 'nani':
-            bot.sendMessage({
-                to: channelID,
-                message: "NANI?!"
-            });
-            break;
-            case 'repo':
-            bot.sendMessage({
-                to: channelID,
-                message: `find me and improve me at ${repo}`
-            });
-            break;
-         }
-     }
-});
+
+bot.login(botSettings.token);
